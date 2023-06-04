35f85cbd86366f933fb42951355e2b994bfd2493
Zho <chlwlgh1011@naver.com>
2023-06-04 21:46:06 +0900

    okaoak

--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,11 @@ class _GetQuestionState extends State<GetQuestion> {
   bool _busyStyleSelected = false;
   bool _funStyleSelected = false;
 
+  bool _partnerSelected = false;
+  bool _childSelected = false;
+  bool _familySelected = false;
+  bool _aloneSelected = false;
+  bool _isDaySelected = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -125,206 +130,239 @@ class _GetQuestionState extends State<GetQuestion> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const Text(
-              "가고싶은 나라나 도시를 입력하세요",
-              style: TextStyle(fontSize: 16),
-            ),
-            TextField(
-              controller: _countryController,
-              inputFormatters: const [
-                // FilteringTextInputFormatter.allow(
-                //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
-                // ),
-              ],
-              decoration: const InputDecoration(
-                hintText: '예) 일본, 파리',
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "가고싶은 나라나 도시를 입력하세요",
+                style: TextStyle(fontSize: 16),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              "여행 기간 선택",
-              style: TextStyle(fontSize: 16),
-            ),
-            TextField(
-              controller: _durationController,
-              inputFormatters: const [
-                // FilteringTextInputFormatter.allow(
-                //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
-                // ),
-              ],
-              decoration: const InputDecoration(
-                hintText: '예) 7일, 2주',
+              TextField(
+                controller: _countryController,
+                inputFormatters: const [
+                  // FilteringTextInputFormatter.allow(
+                  //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
+                  // ),
+                ],
+                decoration: const InputDecoration(
+                  hintText: '예) 일본, 파리',
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              "인원 수",
-              style: TextStyle(fontSize: 16),
-            ),
-            TextField(
-              controller: _peopleController,
-              inputFormatters: const [
-                // FilteringTextInputFormatter.allow(
-                //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
-                // ),
-              ],
-              decoration: const InputDecoration(
-                hintText: '예) 2명, 4인 가족',
+              const SizedBox(height: 16),
+              const Text(
+                "여행 기간 선택",
+                style: TextStyle(fontSize: 16),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              "예산",
-              style: TextStyle(fontSize: 16),
-            ),
-            TextField(
-              controller: _budgetController,
-              inputFormatters: const [
-                // FilteringTextInputFormatter.allow(
-                //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
-                // ),
-              ],
-              decoration: const InputDecoration(
-                hintText: '예) 100만원',
+              TextField(
+                controller: _durationController,
+                inputFormatters: const [
+                  // FilteringTextInputFormatter.allow(
+                  //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
+                  // ),
+                ],
+                decoration: const InputDecoration(
+                  hintText: '예) 7일, 2주',
+                ),
               ),
-            ),
-            const Text(
-              "여행 스타일",
-              style: TextStyle(fontSize: 16),
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _natureStyleSelected,
-                  onChanged: (value) {
-                    setState(() {
-                      _natureStyleSelected = value ?? false;
-                      if (_natureStyleSelected) {
-                        _busyStyleSelected = false;
-                        _funStyleSelected = false;
-                      }
-                    });
-                  },
+              const SizedBox(height: 16),
+              const Text(
+                "인원 수",
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                controller: _peopleController,
+                inputFormatters: const [
+                  // FilteringTextInputFormatter.allow(
+                  //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
+                  // ),
+                ],
+                decoration: const InputDecoration(
+                  hintText: '예) 2명, 4인 가족',
                 ),
-                const Text('자연'),
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _busyStyleSelected,
-                  onChanged: (value) {
-                    setState(() {
-                      _busyStyleSelected = value ?? false;
-                      if (_busyStyleSelected) {
-                        _natureStyleSelected = false;
-                        _funStyleSelected = false;
-                      }
-                    });
-                  },
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "예산",
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                controller: _budgetController,
+                inputFormatters: const [
+                  // FilteringTextInputFormatter.allow(
+                  //   RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ]'),
+                  // ),
+                ],
+                decoration: const InputDecoration(
+                  hintText: '예) 100만원',
                 ),
-                const Text('바쁘게'),
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _funStyleSelected,
-                  onChanged: (value) {
-                    setState(() {
-                      _funStyleSelected = value ?? false;
-                      if (_funStyleSelected) {
-                        _natureStyleSelected = false;
-                        _busyStyleSelected = false;
-                      }
-                    });
-                  },
-                ),
-                const Text('재밌게'),
-              ],
-            ),
-            const Text(
-              "여행 동반자",
-              style: TextStyle(fontSize: 16),
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _natureStyleSelected,
-                  onChanged: (value) {
-                    setState(() {
-                      _natureStyleSelected = value ?? false;
-                      if (_natureStyleSelected) {
-                        _busyStyleSelected = false;
-                        _funStyleSelected = false;
-                      }
-                    });
-                  },
-                ),
-                const Text('자연'),
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _busyStyleSelected,
-                  onChanged: (value) {
-                    setState(() {
-                      _busyStyleSelected = value ?? false;
-                      if (_busyStyleSelected) {
-                        _natureStyleSelected = false;
-                        _funStyleSelected = false;
-                      }
-                    });
-                  },
-                ),
-                const Text('바쁘게'),
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _funStyleSelected,
-                  onChanged: (value) {
-                    setState(() {
-                      _funStyleSelected = value ?? false;
-                      if (_funStyleSelected) {
-                        _natureStyleSelected = false;
-                        _busyStyleSelected = false;
-                      }
-                    });
-                  },
-                ),
-                const Text('재밌게'),
-              ],
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                String country = _countryController.text;
-                String duration = _durationController.text;
-                String people = _peopleController.text;
-                String budget = _budgetController.text;
-                String travelStyle = _natureStyleSelected
-                    ? '자연'
-                    : _busyStyleSelected
-                        ? '바쁘게'
-                        : _funStyleSelected
-                            ? '재밌게'
-                            : '';
-                String prompt =
-                    "I'm going on a trip. You are my tour guide. The travel period I want to go is a total of $duration days. I want to go to $country. The following are the considerations. 1. I personally like $travelStyle 2. I will travel $Partner 3. My budget is $budget Based on this, answer the following questions. Please recommend a schedule for each Day or Hour, indicate the cost of each activity, Acommodation (Travel plans should be formulated in accordance with the budget as much as possible and should not exceed the budget.";
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ResultPage(prompt),
+              ),
+              const Text(
+                "여행 스타일",
+                style: TextStyle(fontSize: 16),
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _natureStyleSelected,
+                    onChanged: (value) {
+                      setState(() {
+                        _natureStyleSelected = value ?? false;
+                        if (_natureStyleSelected) {
+                          _busyStyleSelected = false;
+                          _funStyleSelected = false;
+                        }
+                      });
+                    },
                   ),
-                );
-              },
-              child: const Text("Get result"),
-            ),
-          ],
+                  const Text('자연'),
+                ],
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _busyStyleSelected,
+                    onChanged: (value) {
+                      setState(() {
+                        _busyStyleSelected = value ?? false;
+                        if (_busyStyleSelected) {
+                          _natureStyleSelected = false;
+                          _funStyleSelected = false;
+                        }
+                      });
+                    },
+                  ),
+                  const Text('바쁘게'),
+                ],
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _funStyleSelected,
+                    onChanged: (value) {
+                      setState(() {
+                        _funStyleSelected = value ?? false;
+                        if (_funStyleSelected) {
+                          _natureStyleSelected = false;
+                          _busyStyleSelected = false;
+                        }
+                      });
+                    },
+                  ),
+                  const Text('재밌게'),
+                ],
+              ),
+              const Text(
+                "동행자 선택",
+                style: TextStyle(fontSize: 16),
+              ),
+              CheckboxListTile(
+                title: const Text('연인'),
+                value: _partnerSelected,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _partnerSelected = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text('자녀'),
+                value: _childSelected,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _childSelected = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text('가족'),
+                value: _familySelected,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _familySelected = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text('혼자'),
+                value: _aloneSelected,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _aloneSelected = value!;
+                  });
+                },
+              ),
+              const Text(
+                "일/시간 선택",
+                style: TextStyle(fontSize: 16),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Radio(
+                        value: true,
+                        groupValue: _isDaySelected,
+                        onChanged: (value) {
+                          setState(() {
+                            _isDaySelected = value as bool;
+                          });
+                        },
+                      ),
+                      const Text('일'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Radio(
+                        value: false,
+                        groupValue: _isDaySelected,
+                        onChanged: (value) {
+                          setState(() {
+                            _isDaySelected = value as bool;
+                          });
+                        },
+                      ),
+                      const Text('시간'),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  String country = _countryController.text;
+                  String duration = _durationController.text;
+                  String people = _peopleController.text;
+                  String budget = _budgetController.text;
+                  String travelStyle = _natureStyleSelected
+                      ? '자연'
+                      : _busyStyleSelected
+                          ? '바쁘게'
+                          : _funStyleSelected
+                              ? '재밌게'
+                              : '';
+                  String partner = _partnerSelected
+                      ? '연인과'
+                      : _childSelected
+                          ? '아이와'
+                          : _familySelected
+                              ? '가족과'
+                              : _aloneSelected
+                                  ? '혼자'
+                                  : '';
+                  String prompt =
+                      "I'm going on a trip. You are my tour guide. The travel period I want to go is a total of $duration days. I want to go to $country. The following are the considerations. 1. I personally like $travelStyle 2. I will travel $partner 3. My budget is $budget Based on this, answer the following questions. Please recommend a schedule for each $_isDaySelected, indicate the cost of each activity, Acommodation (Travel plans should be formulated in accordance with the budget as much as possible and should not exceed the budget.";
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ResultPage(prompt),
+                    ),
+                  );
+                },
+                child: const Text("Get result"),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -400,53 +438,53 @@ class _ResultPageState extends State<ResultPage> {
   }
 }
 
-Future<void> getLanguage_papago() async {
-  String clientId = "XmTlLucY96Uw2pp4rj8q";
-  String clientSecret = "9r2hYCiRF5";
-  String contentType = "application/x-www-form-urlencoded; charset=UTF-8";
-  String url = "https://openapi.naver.com/v1/papago/detectLangs";
+// Future<void> getLanguage_papago() async {
+//   String clientId = "XmTlLucY96Uw2pp4rj8q";
+//   String clientSecret = "9r2hYCiRF5";
+//   String contentType = "application/x-www-form-urlencoded; charset=UTF-8";
+//   String url = "https://openapi.naver.com/v1/papago/detectLangs";
 
-  http.Response lan = await http.post(Uri.parse(url), headers: {
-    // 'query': text,
-    'Content-Type': contentType,
-    'X-Naver-Client-Id': clientId,
-    'X-Naver-Client-Secret': clientSecret
-  }, body: {
-    'query': prompt
-  });
-  if (lan.statusCode == 200) {
-    var dataJson = jsonDecode(lan.body);
-    //만약 성공적으로 언어를 받아왔다면 language 변수에 언어가 저장됩니다. (ex: eu, ko, etc..)
-    var language = dataJson['langCode'];
-  } else {
-    print(lan.statusCode);
-  }
-}
+//   http.Response lan = await http.post(Uri.parse(url), headers: {
+//     // 'query': text,
+//     'Content-Type': contentType,
+//     'X-Naver-Client-Id': clientId,
+//     'X-Naver-Client-Secret': clientSecret
+//   }, body: {
+//     'query': prompt
+//   });
+//   if (lan.statusCode == 200) {
+//     var dataJson = jsonDecode(lan.body);
+//     //만약 성공적으로 언어를 받아왔다면 language 변수에 언어가 저장됩니다. (ex: eu, ko, etc..)
+//     var language = dataJson['langCode'];
+//   } else {
+//     print(lan.statusCode);
+//   }
+// }
 
-Future<void> getTranslation_papago() async {
-  String clientId = "XmTlLucY96Uw2pp4rj8q";
-  String clientSecret = "9r2hYCiRF5";
-  String contentType = "application/x-www-form-urlencoded; charset=UTF-8";
-  String url = "https://openapi.naver.com/v1/papago/n2mt";
-  await getLanguage_papago();
+// Future<void> getTranslation_papago() async {
+//   String clientId = "XmTlLucY96Uw2pp4rj8q";
+//   String clientSecret = "9r2hYCiRF5";
+//   String contentType = "application/x-www-form-urlencoded; charset=UTF-8";
+//   String url = "https://openapi.naver.com/v1/papago/n2mt";
+//   await getLanguage_papago();
 
-  http.Response trans = await http.post(
-    Uri.parse(url),
-    headers: {
-      'Content-Type': contentType,
-      'X-Naver-Client-Id': clientId,
-      'X-Naver-Client-Secret': clientSecret
-    },
-    body: {
-      'source': language, //위에서 언어 판별 함수에서 사용한 language 변수
-      'target': "en", //원하는 언어를 선택할 수 있다.
-      'text': "번역할 내용",
-    },
-  );
-  if (trans.statusCode == 200) {
-    var dataJson = jsonDecode(trans.body);
-    var resultPapago = dataJson['message']['result']['translatedText'];
-  } else {
-    print(trans.statusCode);
-  }
-}
+//   http.Response trans = await http.post(
+//     Uri.parse(url),
+//     headers: {
+//       'Content-Type': contentType,
+//       'X-Naver-Client-Id': clientId,
+//       'X-Naver-Client-Secret': clientSecret
+//     },
+//     body: {
+//       'source': language, //위에서 언어 판별 함수에서 사용한 language 변수
+//       'target': "en", //원하는 언어를 선택할 수 있다.
+//       'text': "번역할 내용",
+//     },
+//   );
+//   if (trans.statusCode == 200) {
+//     var dataJson = jsonDecode(trans.body);
+//     var resultPapago = dataJson['message']['result']['translatedText'];
+//   } else {
+//     print(trans.statusCode);
+//   }
+// }
